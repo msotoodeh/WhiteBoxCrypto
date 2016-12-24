af01966216c0255b58fb78e5d9593bcf0f80c096
Mehdi Sotoodeh <mehdisotoodeh@gmail.com>
2016-12-24 15:29:57 -0800

    adding FDE samples

---
+++ FDE/samples/c/test.c
@@ -0,0 +1,59 @@
+/*
+  Copyright (C) 2013 KryptoLogik.com.  All rights reserved.
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+ */
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// --------------------------------------------------------------------------
+// Include the key named 'abc'
+// Accepts cerdit card style inputs (encoding decimal digits)
+// Packed size is 16 (number of digits in plain/cipher text)
+// Non-decimal inputs are taken as formatting characters
+#include "kplus_fde_abc_encrypt.h"
+#include "kplus_fde_abc_decrypt.h"
+// --------------------------------------------------------------------------
+
+int fde_d2_Test(const char *pt, const char *ct)
+{
+    int rc = 0;
+    char tt[80];
+
+    printf("\nPT: %s", pt);
+    printf("\nCT: %s", kplus_fde_abc_encrypt(pt, tt));
+    if (ct && strcmp(tt, ct) != 0)
+    {
+        printf(" -- FAIL (KAT)");
+        rc++;
+    }
+    printf("\nPT? %s", kplus_fde_abc_decrypt(tt, tt));
+    rc += (strcmp(tt, pt) == 0) ? 0 : 1;
+    printf(" -- %s\n", rc ? "FAIL" : "PASS");
+    return rc;
+}
+
+int main (int argc, char ** argv)
+{
+    int rc = 0;
+
+    if (argc > 1)
+        return fde_d2_Test(argv[1], 0);
+
+    rc += fde_d2_Test("1234-5678-9012-3456","6364-9193-8641-7518");
+    rc += fde_d2_Test("0000.0000.0000.0000","5973.0207.8184.2608");
+    rc += fde_d2_Test("0000.0001.0000.0001","8138.0777.0929.3461");
+    rc += fde_d2_Test("0000.0000.1000.0000","2581.9159.0195.6384");
+    rc += fde_d2_Test("0000.0010.0000.0000","1583.2907.4641.3480");
+    rc += fde_d2_Test("1111-1111-1111-1111","4948-3447-7236-5824");
+    rc += fde_d2_Test("1110-1111-1111-1111","3499-0663-3295-3279");
+    rc += fde_d2_Test("9999/9999/9999/9999","7859/6241/7785/7984");
+    return rc;
+}
---
+++ FDE/samples/cs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using kplus.fde.abc;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pt = "1234-5678-9012-3456";
+            if (args.Length > 0) pt = args[0];
+
+            Console.WriteLine("Key name ......... {0}", kplus.fde.abc.RawKey.keyName);
+            Console.WriteLine("Key id ........... 0x{0:x8}", kplus.fde.abc.Format.keyId());
+            Console.WriteLine("Key attributes ... 0x{0:x8}", kplus.fde.abc.Format.keyAttr());
+
+            string ct = kplus.fde.abc.encrypt.transform(pt);
+            string tt = kplus.fde.abc.decrypt.transform(ct);
+
+            Console.WriteLine("Encrypt/Decrypt:");
+			Console.WriteLine("{0} --E--> {1} --D--> {2} : {3}", 
+			                  pt, ct, tt, tt.Equals(pt) ? "PASS" : "FAIL");
+
+            // Switch E and D
+            ct = kplus.fde.abc.decrypt.transform(pt);
+            tt = kplus.fde.abc.encrypt.transform(ct);
+
+            Console.WriteLine("Decrypt/Encrypt:");
+			Console.WriteLine("{0} --D--> {1} --E--> {2} : {3}", 
+			                  pt, ct, tt, tt.Equals(pt) ? "PASS" : "FAIL");
+        }
+    }
+}
